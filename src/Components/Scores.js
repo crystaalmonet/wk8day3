2960e1fe773a3aee48d3cd183f8951233941f69d
crystaalmonet <officialsocialparty@gmail.com>
2023-08-16 00:58:08 -0400

    Submitting homework

---
+++ src/Components/Scores.js
@@ -0,0 +1,25 @@
+
+import React from 'react'
+
+function Scores(scoreData) {
+  console.log(scoreData)
+  return (
+    <div>
+
+
+      {scoreData?.scoreData?.map((score, index) => (
+        <ul>
+          <li>
+            {score.date}
+            <br ></br>
+            {score.score}
+          </li>
+        </ul>
+      )
+      )}
+
+    </div>
+  )
+}
+
+export default Scores;
